012b398dca601afa012b9c778f9f6bcc0773f76c
Victor Cabello <vmeca87@gmail.com>
2024-07-13 19:15:47 -0600

    Clean code for sort

--- tools/sort.py
+++ tools/sort.py
@@ -2,55 +2,91 @@ import os
 import argparse
 import re
 import shutil
+from typing import TypedDict
+from pathlib import Path
 
 
-def isNotDuplicated(filename: str) -> bool:
-    """When a fil already exitst the
-    system add a suffix like "(#)" where # is
-    a number.
-
-    This function return False if the filename
-    contains a pattern like "(1)" or "(2)" or
-    any number between parentesis.
-
-    >>> isNotDuplicated('file.exe')
-    True
-    >>> isNotDuplicated("otherFile (1) .pdf")
-    False
-    """
-    return not bool(re.search('(\\d+)', filename))
+def main():
+    args = get_cli_args()
+    if args['unittest']:
+        runDocTests()
+        return
+    movePDF(args['target'], args['source'])
 
 
-def isPDF(filename: str) -> bool:
-    """Return true if the filname ends with '.pdf'
-
-    >>> isPDF('file.exe')
-    False
-    >>> isPDF("otherFile.pdf")
-    True
-    >>> isPDF('malfurmedped')
-    False
-    """
-    return filename.endswith('.pdf')
+class CLIArgsType(TypedDict):
+    unittest: bool
+    target: str
+    source: str
 
 
-def isFile(filename: str) -> bool:
-    """Return true if the string represents a file
-    that exits"""
-    return os.path.isfile(filename)
+def dir_path(path: str) -> str:
+    if os.path.exists(path):
+        return path
+    else:
+        raise NotADirectoryError(path)
 
 
-def argumentsInit() -> argparse.ArgumentParser:
+def get_cli_args() -> CLIArgsType:
     description = """
     This program move all pdf file into a folder and delete
     duplicated.
     """
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("-t", "--test",
+    parser.add_argument("-ut", "--unittest",
                         help="run unit test",
                         action="store_true")
+    parser.add_argument("--target",
+                        type=dir_path,
+                        help="path to target directory",
+                        default='./pdfs',
+                        action="store")
+    parser.add_argument("source",
+                        type=dir_path,
+                        help="path to source directory",
+                        nargs='?',
+                        default='.',
+                        action="store")
     args = parser.parse_args()
-    return args
+    return vars(args)
+
+
+def runDocTests() -> None:
+    """
+    Runs the doctests defined in the module.
+
+    This function uses `doctest.testmod()` to automatically find and run all
+    the doctests in the module. Doctests are snippets of interactive Python
+    sessions that are embedded in the docstrings of functions, classes, and
+    modules.
+
+    Doctests are a convenient way to both test your code and provide examples
+    of how it works.
+
+    """
+    import doctest
+    doctest.testmod()
+
+
+def movePDF(trgDirPath: str, srcDirPath: str) -> None:
+    createDir(trgDirPath)
+    print(srcDirPath)
+    files = getFiles(srcDirPath)
+    print(files)
+    for file in files:
+        shutil.copy2(file, trgDirPath)
+
+
+def isNotDuplicated(filename: Path) -> bool:
+    return not bool(re.search('(\\d+)', filename.stem))
+
+
+def isPDF(filename: Path) -> bool:
+    return filename.suffix == '.pdf'
+
+
+def isFile(filename: Path) -> bool:
+    return os.path.isfile(filename)
 
 
 class MultiFilter:
@@ -63,10 +99,11 @@ class MultiFilter:
         return all(myFilter(toFilter) for myFilter in self.filters)
 
 
-def getFiles() -> list[str]:
+def getFiles(srcDirPath: str) -> list[str]:
     filters = (isPDF, isFile, isNotDuplicated)
     multiFilter = MultiFilter(filters)
-    files = filter(multiFilter, os.listdir('.'))
+    p = Path(srcDirPath)
+    files = filter(multiFilter, list(p.iterdir()))
     return list(files)
 
 
@@ -75,21 +112,6 @@ def createDir(dirName: str) -> None:
         return
 
     os.mkdir(dirName)
-
-
-def main():
-    args = argumentsInit()
-
-    if args.test:
-        import doctest
-        doctest.testmod()
-        return
-
-    outputDir = 'pdfs'
-    createDir(outputDir)
-    files = getFiles()
-    for file in files:
-        shutil.copy2(file, outputDir)
 
 
 if __name__ == "__main__":
